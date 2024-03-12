62fc7c109b3a5047a4639297543852a75a36c0e9
Sourabh20022002 <Sourabh52.singh@gmail.com>
2024-03-12 20:51:12 +0530

    new_pdf44

--- code.py
+++ code.py
@@ -16,8 +16,7 @@ app = FastAPI()
 
 api_key = os.environ.get('OPENAI_API_KEY')
 openai.api_key = api_key
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+os.getenv("OPENAI_API_KEY")
 WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
 WEAVIATE_URL = os.getenv("WEAVIATE_URL")
 auth_config = weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY)
